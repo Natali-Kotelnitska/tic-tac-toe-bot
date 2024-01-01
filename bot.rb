9a81ea19fdee813827a2d42f743150c9538ae3f3
Natali Kotelnytska <nkotelnicka@gmail.com>
2024-01-01 02:50:00 +0200

    Refactor: add minimax algorithm

--- bot.rb
+++ bot.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'dotenv/load'
 require 'telegram/bot'
 require_relative 'game'
 
+# Class representing a TelegramBot.
 class TelegramBot
   def initialize
     @game = nil
@@ -33,13 +36,16 @@ class TelegramBot
       if @game
         x, y = message.data.split(',').map(&:to_i)
         @game.make_move(x, y)
-        if @play_with_computer && @game.instance_variable_get(:@current_player) == @game.instance_variable_get(:@player)
-          x, y = @game.computer_move
-          @game.make_move(x, y)
-        end
         game_status = @game.game_over?
         if game_status
           handle_game_over(bot, message, game_status)
+        elsif @play_with_computer && !game_status
+          x, y = @game.computer_move
+          @game.make_move(x, y)
+          game_status = @game.game_over?
+          if game_status
+            handle_game_over(bot, message, game_status)
+          end
         end
       end
     end
@@ -61,18 +67,28 @@ class TelegramBot
     end
   end
 
-  # Methods for handling different message cases
-  def send_initial_message(bot, message)
-    user_full_name = "#{message.from.first_name} #{message.from.last_name}"
-    bot.api.send_message(chat_id: message.from.id, text: "Hello #{user_full_name} 👋")
-    bot.api.send_message(chat_id: message.chat.id, text: "I'm your Tic Tac Toe bot. You can play a game of Tic Tac Toe with a friend or with the computer. To start a new game, please choose 'With player' or 'With computer'.")
-    question = 'How do you want to play?'
-    answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(
+  def determine_winner
+    current_player = @game.instance_variable_get(:@current_player)
+    player = @game.instance_variable_get(:@player)
+    opponent = @game.instance_variable_get(:@opponent)
+    current_player == player ? opponent : player
+  end
+
+  def create_keyboard
+    Telegram::Bot::Types::ReplyKeyboardMarkup.new(
       keyboard: [
-        [{ text: 'With player' }, { text: 'With computer' }],
+        [{ text: 'With player' }, { text: 'With computer' }]
       ],
       one_time_keyboard: true
     )
+  end
+
+  def send_initial_message(bot, message)
+    user_full_name = "#{message.from.first_name} #{message.from.last_name}"
+    bot.api.send_message(chat_id: message.from.id, text: "Hello #{user_full_name} 👋")
+    bot.api.send_message(chat_id: message.chat.id, text: "I'm your Tic Tac Toe bot.")
+    question = 'How do you want to play?'
+    answers = create_keyboard
     bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
   end
 
@@ -80,7 +96,7 @@ class TelegramBot
     if game_status == :draw
       bot.api.send_message(chat_id: message.message.chat.id, text: "Game over. It's a draw.")
     else
-      winner = @game.instance_variable_get(:@current_player) == @game.instance_variable_get(:@player) ? @game.instance_variable_get(:@opponent) : @game.instance_variable_get(:@player)
+      winner = determine_winner
       bot.api.send_message(chat_id: message.message.chat.id, text: "Game over. The winner is #{winner}.")
     end
     send_new_game_button(bot, message)
--- game.rb
+++ game.rb
@@ -6,7 +6,7 @@ class Game
     @board = Array.new(3) { Array.new(3, '-') }
     @message = message
     @bot = bot
-    @player = '🎅' # Санта Клаус
+    @player = '🎅' # Санта
     @opponent = '🧛' # Вампір
     @current_player = @player
   end
@@ -21,33 +21,86 @@ class Game
     print_board
   end
 
+  def minimax(board, depth, is_maximizing)
+    winner = check_winner(board)
+
+    return 10 - depth if winner == :opponent
+    return -10 + depth if winner == :player
+    return 0 if winner == :draw
+
+    best_score = is_maximizing ? -Float::INFINITY : Float::INFINITY
+    current_player = is_maximizing ? @opponent : @player
+    next_maximizing = !is_maximizing
+
+    evaluate_board(board, current_player, depth, next_maximizing, best_score)
+  end
+
+  def evaluate_board(board, current_player, depth, next_maximizing, best_score)
+    board.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        next unless cell == '-'
+
+        board[i][j] = current_player
+        score = minimax(board, depth + 1, next_maximizing)
+        board[i][j] = '-'
+
+        best_score = next_maximizing ? [score, best_score].min : [score, best_score].max
+      end
+    end
+    best_score
+  end
+
   def computer_move
+    best_score = -Float::INFINITY
+    move = nil
+
     @board.each_with_index do |row, i|
       row.each_with_index do |cell, j|
         if cell == '-'
-          return [i, j]
+          @board[i][j] = @opponent
+          score = minimax(@board, 0, false)
+          @board[i][j] = '-'
+          if score > best_score
+            best_score = score
+            move = [i, j]
+          end
         end
       end
     end
+
+    move
   end
 
-  def game_over?
+  def check_winner(board)
     # Перевірка рядків
-    @board.each do |row|
-      return true if row.uniq.length == 1 && row[0] != '-'
+    board.each do |row|
+      return :opponent if row.all? { |cell| cell == @opponent }
+      return :player if row.all? { |cell| cell == @player }
     end
 
     # Перевірка стовпців
-    @board.transpose.each do |col|
-      return true if col.uniq.length == 1 && col[0] != '-'
+    board.transpose.each do |col|
+      return :opponent if col.all? { |cell| cell == @opponent }
+      return :player if col.all? { |cell| cell == @player }
     end
 
     # Перевірка діагоналей
-    return true if [@board[0][0], @board[1][1], @board[2][2]].uniq.length == 1 && @board[0][0] != '-'
-    return true if [@board[0][2], @board[1][1], @board[2][0]].uniq.length == 1 && @board[0][2] != '-'
+    return :opponent if [board[0][0], board[1][1], board[2][2]].all? { |cell| cell == @opponent }
+    return :player if [board[0][0], board[1][1], board[2][2]].all? { |cell| cell == @player }
+    return :opponent if [board[0][2], board[1][1], board[2][0]].all? { |cell| cell == @opponent }
+    return :player if [board[0][2], board[1][1], board[2][0]].all? { |cell| cell == @player }
 
     # Перевірка, чи всі клітинки заповнені
-    return :draw unless @board.flatten.include?('-')
+    return :draw unless board.flatten.include?('-')
+
+    # Якщо гра ще не закінчилася, повертаємо nil
+    nil
+  end
+
+  def game_over?
+    winner = check_winner(@board)
+    return true if [:player, :opponent].include?(winner)
+    return :draw if winner == :draw
 
     false
   end
